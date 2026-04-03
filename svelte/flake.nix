cef812b58a8bc9762fa8f48eea988232e2df263a
Nanozuki Crows <nanozuki.crows@gmail.com>
2026-04-03 15:27:11 +0200

    feat: nodePackages migration

--- svelte/flake.nix
+++ svelte/flake.nix
@@ -5,22 +5,36 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs =
+    inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            litecli
-            nodePackages.nodejs
-            nodePackages.pnpm
-            nodePackages.svelte-language-server
-          ];
-          shellHook = ''
-            export ESLINT_USE_FLAT_CONFIG="true"
-          '';
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              litecli
+              nodejs
+              pnpm
+              svelte-language-server
+            ];
+            shellHook = ''
+              export ESLINT_USE_FLAT_CONFIG="true"
+            '';
+          };
         };
-      };
     };
 }
-
